c4c65c10ed397df55e841ead39fda85ee0755fc8
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-26 10:43:30 +0530

    Create Card component, populate data, and implement search functionality for quizzes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
   import q from "./data/quizes.json";
-  import {ref} from "vue";
+  import {ref, watch} from "vue";
+  import Card from "./components/Card.vue"
 
   const quizes = ref(q);
   const search = ref("")
+
+  watch(search, () => {
+    quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()));
+  });
 
 </script>
 
@@ -11,17 +16,11 @@
   <div class="container">
     <header class="header">
       <h1>Quizes</h1>
-      <input v-mode="search" type="text" placeholder="Search..." class="search-input">
+      <input v-model="search" type="text" placeholder="Search..." class="search-input">
     </header>
   </div>
   <div class="options-container">
-    <div v-for="quiz in quizes" :key="quiz.id" class="card">
-      <img :src="quiz.img" alt="quiz.name">
-      <div class="card-text">
-        <h2>{{ quiz.name }}</h2>
-        <p>{{ quiz.questions.lenght }}</p>
-      </div>
-    </div>
+    <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
   </div>
 </template>
 
@@ -60,38 +59,5 @@
   flex-wrap: wrap;
   gap: 20px;
   margin-top: 20px;
-}
-
-.card {
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
-  width: calc(33.333% - 20px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-}
-
-.card img {
-  width: 100%;
-  height: auto;
-}
-
-.card-text {
-  padding: 20px;
-}
-
-.card-text h2 {
-  margin: 0 0 10px;
-  font-size: 1.5rem;
-  color: #333;
-}
-
-.card-text p {
-  margin: 0;
-  color: #666;
-  font-size: 1rem;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<script setup>
+	import {defineProps} from "vue"
+
+	const { quiz } = defineProps(['quiz'])
+</script>
+
+<template>
+	<div class="card">
+      <img :src="quiz.img" alt="">
+      <div class="card-text">
+        <h2>{{ quiz.name }}</h2>
+        <p>{{ quiz.questions.length }}</p>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+	
+.card {
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  width: calc(33.333% - 20px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.card img {
+  width: 100%;
+  height: auto;
+}
+
+.card-text {
+  padding: 20px;
+}
+
+.card-text h2 {
+  margin: 0 0 10px;
+  font-size: 1.5rem;
+  color: #333;
+}
+
+.card-text p {
+  margin: 0;
+  color: #666;
+  font-size: 1rem;
+}
+
+</style>
